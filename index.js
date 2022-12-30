528d97a24430e926d6daf2e4ca46a3be905f9fcf
Nickita Danilovich <nickitadanilovich@gmail.com>
2022-12-30 10:28:08 -0500

    commit tree b698b6278b5ecc47ba9109d6f911a78b24524b9e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name: "Daniel",
+    street: "Ave R",
+}
+
+
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employees = {...employee};
+    employees[key] = value;
+    return employees
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const worker = {...employee};
+    delete worker[key];
+    return worker;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
